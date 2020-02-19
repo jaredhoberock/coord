ee8cf1a8d4fd87b2d04e24aaf270e496abd8815d
Jared Hoberock <jaredhoberock@gmail.com>
2020-02-19 16:15:31 -0600

    Introduce and test shape_first

--- coord/shape.hpp
+++ coord/shape.hpp
@@ -7,7 +7,10 @@
 #include "shape/make_shape.hpp"
 #include "shape/shape_append.hpp"
 #include "shape/shape_element.hpp"
+#include "shape/shape_first.hpp"
+#include "shape/shape_first_element.hpp"
 #include "shape/shape_last.hpp"
+#include "shape/shape_last_element.hpp"
 #include "shape/shape_size.hpp"
 #include "shape/shape_truncate.hpp"
 
