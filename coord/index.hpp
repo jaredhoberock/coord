7d378f1a9c77c7b0faf57ca4bcc841bcf92cdcf3
Jared Hoberock <jaredhoberock@gmail.com>
2020-02-26 16:43:07 -0600

    Update index.hpp header

--- coord/index.hpp
+++ coord/index.hpp
@@ -2,7 +2,17 @@
 
 #include "detail/prologue.hpp"
 
+#include "index/colexicographic_rank.hpp"
+#include "index/colexicographic_rank_to_index.hpp"
+#include "index/first_index_element.hpp"
+#include "index/index_cast.hpp"
+#include "index/index_element.hpp"
+#include "index/index_first.hpp"
+#include "index/index_size.hpp"
 #include "index/index_space_size.hpp"
+#include "index/is_index.hpp"
+#include "index/lexicographic_rank.hpp"
+#include "index/make_index.hpp"
 
 #include "detail/epilogue.hpp"
 
