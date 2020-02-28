f32cdec20e64fb7d1abe201dfc6467db35285d04
Jared Hoberock <jaredhoberock@gmail.com>
2020-02-28 16:54:54 -0600

    Add missing #include

--- coord/detail/epilogue.hpp
+++ coord/detail/epilogue.hpp
@@ -14,6 +14,7 @@
 // include configuration headers
 
 #include "config/annotation.hpp"
+#include "config/concat.hpp"
 #include "config/exec_check_disable.hpp"
 #include "config/namespace.hpp"
 #include "config/requires.hpp"
