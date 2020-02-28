a4bc18ad71e40ee22dccfd50b9bdd81f7aff4b05
Jared Hoberock <jaredhoberock@gmail.com>
2020-02-28 17:10:16 -0600

    Test for undefined preprocessor symbols

---
+++ tests/preprocessor.hpp
@@ -0,0 +1,56 @@
+#include <coord/index.hpp>
+#include <coord/lattice.hpp>
+#include <coord/point.hpp>
+#include <coord/shape.hpp>
+
+#ifdef COORD_INCLUDE_LEVEL
+#error COORD_INCLUDE_LEVEL defined in client code.
+#endif
+
+#ifdef COORD_ANNOTATION
+#error COORD_ANNOTATION defined in client code.
+#endif
+
+#ifdef COORD_CONCATENATE
+#error COORD_CONCATENATE defined in client code.
+#endif
+
+#ifdef COORD_CONCATENATE_IMPL
+#error COORD_CONCATENATE_IMPL defined in client code.
+#endif
+
+#ifdef COORD_EXEC_CHECK_DISABLE
+#error COORD_EXEC_CHECK_DISABLE defined in client code.
+#endif
+
+#ifdef COORD_MAKE_UNIQUE
+#error COORD_MAKE_UNIQUE defined in client code.
+#endif
+
+#ifdef COORD_NAMESPACE
+#error COORD_NAMESPACE defined in client code.
+#endif
+
+#ifdef COORD_NAMESPACE_CLOSE_BRACE
+#error COORD_NAMESPACE_CLOSE_BRACE defined in client code.
+#endif
+
+#ifdef COORD_NAMESPACE_OPEN_BRACE
+#error COORD_NAMESPACE_OPEN_BRACE defined in client code.
+#endif
+
+#ifdef COORD_NAMESPACE_NEEDS_UNDEF
+#error COORD_NAMESPACE_NEEDS_UNDEF defined in client code.
+#endif
+
+#ifdef COORD_REQUIRES
+#error COORD_REQUIRES defined in client code.
+#endif
+
+#ifdef COORD_REQUIRES_IMPL
+#error COORD_REQUIRES_IMPL defined in client code.
+#endif
+
+
+void test_preprocessor() {}
+
