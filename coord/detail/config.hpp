e44ee9344cf740800f17dcdfc63756e8b227990d
Jared Hoberock <jaredhoberock@gmail.com>
2020-02-28 17:21:31 -0600

    Eliminate superfluous config header

--- coord/detail/config.hpp
+++ coord/detail/config.hpp
@@ -8,7 +8,6 @@
 // detail/config directory.
 
 #include "config/annotation.hpp"
-#include "config/concat.hpp"
 #include "config/exec_check_disable.hpp"
 #include "config/namespace.hpp"
 #include "config/requires.hpp"
