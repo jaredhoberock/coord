ea985ad6b42413cb57efb8b379f19831d501acfa
Jared Hoberock <jaredhoberock@gmail.com>
2020-02-28 17:09:51 -0600

    Eliminate non-existent #includes

--- coord/shape.hpp
+++ coord/shape.hpp
@@ -5,15 +5,9 @@
 #include "shape/common_shape.hpp"
 #include "shape/is_shape.hpp"
 #include "shape/make_shape.hpp"
-#include "shape/shape_append.hpp"
 #include "shape/shape_cast.hpp"
 #include "shape/shape_element.hpp"
-#include "shape/shape_first.hpp"
-#include "shape/shape_first_element.hpp"
-#include "shape/shape_last.hpp"
-#include "shape/shape_last_element.hpp"
 #include "shape/shape_size.hpp"
-#include "shape/shape_truncate.hpp"
 
 #include "detail/epilogue.hpp"
 
