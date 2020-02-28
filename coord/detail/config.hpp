d33606f24cceaac8168db2b64c030028db1ba232
Jared Hoberock <jaredhoberock@gmail.com>
2020-02-28 17:20:06 -0600

    Add notes to config.hpp

--- coord/detail/config.hpp
+++ coord/detail/config.hpp
@@ -1,5 +1,12 @@
 // note that this header file is special and does not use #pragma once
 
+// This header #includes all header files underneath the detail/config directory.
+// The only headers that should #include this file are detail/prologue.hpp and detail/epilogue.hpp
+//
+// A simple way to redefine configuration macros like COORD_NAMESPACE et al is to replace this
+// header file with one containing custom definitions for all macros defined beneath the
+// detail/config directory.
+
 #include "config/annotation.hpp"
 #include "config/concat.hpp"
 #include "config/exec_check_disable.hpp"
