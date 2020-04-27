8fe5bdcbda8739f0de73fdede2db1a64bfc739a3
Jared Hoberock <jaredhoberock@gmail.com>
2020-04-27 16:27:46 -0500

    is_coordinate -> is_coordinal

--- tests/coordinate/is_coordinal.hpp
+++ tests/coordinate/is_coordinal.hpp
@@ -1,7 +1,7 @@
 #include <utility>
 #include <tuple>
 #include <array>
-#include <coord/coordinate/is_coordinate.hpp>
+#include <coord/coordinate/is_coordinal.hpp>
 
 
 struct has_member_functions
@@ -21,38 +21,38 @@ struct has_member_functions
 };
 
 
-void test_is_coordinate()
+void test_is_coordinal()
 {
   // test some coordinates
-  static_assert(coord::is_coordinate<has_member_functions>::value, "Error.");
+  static_assert(coord::is_coordinal<has_member_functions>::value, "Error.");
 
-  static_assert(coord::is_coordinate<int>::value, "Error.");
-  static_assert(coord::is_coordinate<unsigned int>::value, "Error.");
-  static_assert(coord::is_coordinate<std::size_t>::value, "Error.");
-  static_assert(coord::is_coordinate<std::tuple<char>>::value, "Error.");
+  static_assert(coord::is_coordinal<int>::value, "Error.");
+  static_assert(coord::is_coordinal<unsigned int>::value, "Error.");
+  static_assert(coord::is_coordinal<std::size_t>::value, "Error.");
+  static_assert(coord::is_coordinal<std::tuple<char>>::value, "Error.");
 
-  static_assert(coord::is_coordinate<float>::value, "Error.");
-  static_assert(coord::is_coordinate<double>::value, "Error.");
-  static_assert(coord::is_coordinate<std::tuple<float>>::value, "Error.");
-  static_assert(coord::is_coordinate<std::tuple<double>>::value, "Error.");
+  static_assert(coord::is_coordinal<float>::value, "Error.");
+  static_assert(coord::is_coordinal<double>::value, "Error.");
+  static_assert(coord::is_coordinal<std::tuple<float>>::value, "Error.");
+  static_assert(coord::is_coordinal<std::tuple<double>>::value, "Error.");
 
-  static_assert(coord::is_coordinate<std::pair<int,int>>::value, "Error.");
-  static_assert(coord::is_coordinate<std::tuple<int>>::value, "Error.");
-  static_assert(coord::is_coordinate<std::tuple<int,unsigned int>>::value, "Error.");
-  static_assert(coord::is_coordinate<std::tuple<int,unsigned int,std::size_t>>::value, "Error.");
-  static_assert(coord::is_coordinate<std::array<std::size_t,3>>::value, "Error.");
+  static_assert(coord::is_coordinal<std::pair<int,int>>::value, "Error.");
+  static_assert(coord::is_coordinal<std::tuple<int>>::value, "Error.");
+  static_assert(coord::is_coordinal<std::tuple<int,unsigned int>>::value, "Error.");
+  static_assert(coord::is_coordinal<std::tuple<int,unsigned int,std::size_t>>::value, "Error.");
+  static_assert(coord::is_coordinal<std::array<std::size_t,3>>::value, "Error.");
 
   using int2 = std::pair<int,int>;
   using uint3 = std::tuple<unsigned int, unsigned int, unsigned int>;
 
-  static_assert(coord::is_coordinate<std::pair<int2,uint3>>::value, "Error.");
-  static_assert(coord::is_coordinate<std::tuple<int2,uint3,std::size_t>>::value, "Error.");
+  static_assert(coord::is_coordinal<std::pair<int2,uint3>>::value, "Error.");
+  static_assert(coord::is_coordinal<std::tuple<int2,uint3,std::size_t>>::value, "Error.");
 
   using uint2x3 = std::pair<uint3,uint3>;
-  static_assert(coord::is_coordinate<uint2x3>::value, "Error.");
+  static_assert(coord::is_coordinal<uint2x3>::value, "Error.");
 
   // test some non-coordinates
-  static_assert(!coord::is_coordinate<std::tuple<int, char*>>::value, "Error.");
-  static_assert(!coord::is_coordinate<std::tuple<float&,float>>::value, "Error.");
+  static_assert(!coord::is_coordinal<std::tuple<int, char*>>::value, "Error.");
+  static_assert(!coord::is_coordinal<std::tuple<float&,float>>::value, "Error.");
 }
 
