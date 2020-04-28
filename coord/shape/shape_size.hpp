2babe0b9c886e8a20a6692ab6a4efc246e94f831
Jared Hoberock <jaredhoberock@gmail.com>
2020-04-28 12:00:28 -0500

    Implement shape_size in terms of rank

--- coord/shape/shape_size.hpp
+++ coord/shape/shape_size.hpp
@@ -28,47 +28,19 @@
 
 #include "../detail/prologue.hpp"
 
-
-#include <tuple>
+#include <cstdint>
 #include <type_traits>
+#include "../coordinate/rank.hpp"
 
 
 COORD_NAMESPACE_OPEN_BRACE
 
 
-namespace detail
-{
-
-// shape_size<i,Shape> is a type trait which returns the
-// number of elements in a Shape.
-//
-// A Shape is either:
-// 1. An integral type or
-// 2. A tuple of Shapes
-
-
-// case 2: Shape is a Tuple-like type
-template<class Shape, class Enable = void>
-struct shape_size_impl : std::tuple_size<Shape> {};
-
-
-// case 1: Shape is an integral type.
 template<class Shape>
-struct shape_size_impl<
-  Shape,
-  typename std::enable_if<
-    std::is_integral<Shape>::value
-  >::type
-> : std::integral_constant<std::size_t, 1>
-{
-};
-
-
-} // end detail
-
-
-template<class Shape>
-struct shape_size : std::integral_constant<size_t, detail::shape_size_impl<Shape>::value> {};
+using shape_size = std::integral_constant<
+  std::size_t,
+  std::is_integral<Shape>::value ? 1 : rank_v<Shape>
+>;
 
 
 COORD_NAMESPACE_CLOSE_BRACE
