fb8a6ae174036e63276ba11b8ebfa9f5ab6a69a7
Jared Hoberock <jaredhoberock@gmail.com>
2020-04-28 11:50:54 -0500

    Make is_shape a synonym for is_discrete

--- tests/shape/is_shape.hpp
+++ tests/shape/is_shape.hpp
@@ -2,6 +2,7 @@
 #include <tuple>
 #include <array>
 #include <coord/shape/is_shape.hpp>
+#include <coord/point.hpp>
 
 void test_is_shape()
 {
@@ -11,14 +12,17 @@ void test_is_shape()
   static_assert(coord::is_shape<std::size_t>::value, "Error.");
   static_assert(coord::is_shape<std::tuple<char>>::value, "Error.");
 
-  static_assert(coord::is_shape<std::pair<int,int>>::value, "Error.");
+  using int2 = std::pair<int,int>;
+  using uint3 = std::tuple<unsigned int, unsigned int, unsigned int>;
+
+  static_assert(coord::is_shape<int2>::value, "Error.");
+  static_assert(coord::is_shape<coord::int2>::value, "Error.");
+  static_assert(coord::is_shape<uint2>::value, "Error.");
+  static_assert(coord::is_shape<coord::uint2>::value, "Error.");
   static_assert(coord::is_shape<std::tuple<int>>::value, "Error.");
   static_assert(coord::is_shape<std::tuple<int,unsigned int>>::value, "Error.");
   static_assert(coord::is_shape<std::tuple<int,unsigned int,std::size_t>>::value, "Error.");
   static_assert(coord::is_shape<std::array<std::size_t,3>>::value, "Error.");
-
-  using int2 = std::pair<int,int>;
-  using uint3 = std::tuple<unsigned int, unsigned int, unsigned int>;
 
   static_assert(coord::is_shape<std::pair<int2,uint3>>::value, "Error.");
   static_assert(coord::is_shape<std::tuple<int2,uint3,std::size_t>>::value, "Error.");
